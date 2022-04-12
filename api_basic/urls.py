2ddddb39002401d6fd37cdb1ec5cf8031c185e77
Akande Al-ameen <longlasting1805@gmail.com>
2022-04-12 11:42:03 +0000

    generic api && mixins

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, articleApiView, articleDetailApi, GenericApiView
 
 urlpatterns = [
-    path('article/', article_list),
-    path('detail/<int:pk>/', article_detail)
+    # path('article/', article_list),
+    path('article/', articleApiView.as_view()),
+    # path('detail/<int:pk>/', article_detail)
+    path('detail/<int:id>/', articleDetailApi.as_view()),
+    path('generic/article/', GenericApiView.as_view())
 ]
